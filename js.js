497733a7ac9a7276298c2bb0b2dab3fa001504fe
Scott Warner <scottbwarner@gmail.com>
2015-10-07 17:10:13 -0400

    first commit

---
+++ js.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+
+    console.log("ready");
+    $('.square').on('click', function() {
+        console.log("this is " + $('.square').index(this));
+
+    $('.square').addClass('yellow');
+
+    });
+
+
+
+
+
+// });
